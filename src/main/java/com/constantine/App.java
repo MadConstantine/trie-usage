df65e2bd55b9f6fa502434e5d01c645e7bb148e4
Constantine <ext.ksafutin@vivid.money>
2020-07-27 02:24:02 +0400

    wip

--- src/main/java/com/constantine/App.java
+++ src/main/java/com/constantine/App.java
@@ -12,20 +12,13 @@ public class App {
         LocalTime start = LocalTime.now();
         System.out.println("Start time: " + start);
 
-//        long count = 0;
         IpTrie trie = new IpTrie();
 
         try (BufferedReader buffer = new BufferedReader(new FileReader(args[0]))) {
             for(String line; (line = buffer.readLine()) != null; ) {
                 trie.storeAddress(line);
-//                count++;
-//                System.out.println("Addresses stored: " + count);
             }
         }
-
-//        try (Stream<String> stream = Files.lines(Paths.get(args[0]))) {
-//            stream.forEach(trie::storeAddress);
-//        }
 
         System.out.println("Start counting...");
         System.out.println("Number of unique addresses: " + trie.countUniqueAddresses());
